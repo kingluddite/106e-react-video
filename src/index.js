41602c49db1e53dc7e23ebe9dab57a13ac851bb2
kindluddite <howley.phil@gmail.com>
2018-02-15 16:02:54 -0800

    convert to class APP component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/SearchBar';
 
-const GOOGLE_API_KEY = '???';
+const GOOGLE_API_KEY = 'AIzaSyDUAMBnBMyKmVCjKzKMTX5WmAvbAF2sq8Q';
 
+YTSearch({ key: GOOGLE_API_KEY, term: 'soccer' }, data => {
+  console.log(data);
+});
 // Create a new Component. This Component should produce some HTML
 const App = function() {
   return (
